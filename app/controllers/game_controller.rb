01475c120dd432b644f70c0daea17dbb56c0646a
zunerz671 <zunerz671@gmail.com>
2025-10-08 20:54:38 +1000

    created game  controller and CRUD actions with index.

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,48 @@
+class GameController < ApplicationController
+  def index
+    games = Game.all
+
+    render json: games
+  end
+
+  def create
+    game = Game.new(
+      team1: params[:team1],
+      team2: params[:team2],
+      team1score: params[:team1score],
+      team2score: params[:team2score]
+    )
+
+    if game.save
+      render json: game
+    else
+      render json: game.errors, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    game = Game.find(params[:id])
+
+    render json: game
+  end
+
+  def update
+    game = Game.find(params[:id])
+    game.update(
+      team1: params[:team1],
+      team2: params[:team2],
+      team1score: params[:team1score],
+      team2score: params[:team2score]
+    )
+
+    render json: game
+  end
+
+  def delete
+    game = Game.find(params[:id])
+
+    game.destroy
+
+    render json: { message: "Game deleted..." }
+  end
+end
