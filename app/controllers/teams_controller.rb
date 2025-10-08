82486f8e9d9df45bbe087da9861bb1f4e14fba46
zunerz671 <zunerz671@gmail.com>
2025-10-08 20:48:39 +1000

    created teams controller and CRUD actions with index.

---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,46 @@
+class TeamsController < ApplicationController
+  def index
+    teams = Team.all
+
+    render json: teams
+  end
+
+  def create
+    team = Team.new(
+      name: params[:name],
+      win: params[:win] || 0,
+      loss: params[:loss] || 0
+    )
+
+    if team.save
+      render json: team
+    else
+      render json: team.errors, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    team = Team.find(params[:id])
+
+    render json: team
+  end
+
+  def update
+    team = Team.find(params[:id])
+    team.update(
+      name: params[:name],
+      win: params[:win],
+      loss: params[:loss]
+    )
+
+    render json: team
+  end
+
+  def delete
+    team = Team.find(params[:id])
+
+    team.destroy
+
+    render json: { message: "Team deleted..." }
+  end
+end
