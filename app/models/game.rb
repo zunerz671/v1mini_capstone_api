b9438ea809365e775f798b7eca8ca5ddc0d78c8e
zunerz671 <zunerz671@gmail.com>
2025-10-09 15:52:10 +1000

    adde validations for team and game models.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,14 @@
 class Game < ApplicationRecord
+  validates :team1, presence: true
+  validates :team2, presence: true
+  validates :team1score, presence: true
+  validates :team2score, presence: true
+
+  validate :team1_cannot_equal_team2
+
+  def team1_cannot_equal_team2
+    if team1 == team2
+      errors.add(:team1, "team1 and team2 names cannot be the same.")
+    end
+  end
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,4 @@
 class Team < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
