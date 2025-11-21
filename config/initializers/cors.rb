ce0ea9add4b561565daef4353e26882e1bb82e14
zunerz671 <zunerz671@gmail.com>
2025-11-21 19:05:34 +1000

    Updated for deployment to render.com.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:5173"
-    resource "*", headers: :any, credentials: true, methods: [ :get, :post, :patch, :put, :delete ]
+    origins "http://localhost:5173"
+    resource "*",
+             headers: :any,
+             methods: %i[get post put patch delete options head],
+             credentials: true
   end
 end
