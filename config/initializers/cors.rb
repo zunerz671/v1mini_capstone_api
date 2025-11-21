949ecad1a4d5a576cd6a3ad7759f32626a4a3d99
zunerz671 <zunerz671@gmail.com>
2025-11-21 19:29:09 +1000

    Updated cors with website address.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173"
+    origins "http://localhost:5173", "https://jo-mini-capstone.netlify.app"
     resource "*",
              headers: :any,
              methods: %i[get post put patch delete options head],
